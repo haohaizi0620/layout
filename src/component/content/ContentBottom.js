3d999ba3444488300b5e387ac63522e8fae13e76
ljy <lijingyu1538@gmail.com>
2020-03-30 14:32:52 +0800

    缩放滑动条数字格式调整

--- src/component/content/ContentBottom.js
+++ src/component/content/ContentBottom.js
@@ -6,49 +6,52 @@
  * @Description: In User Settings Edit
  * @FilePath: \layout\src\component\otherLayer\ContentBottom.js
  */
-import React, { Component } from 'react';
-import {
-    InputNumber,
-    Slider,
-  } from 'antd';
-import './ContentBottom.scss';
+import React, { Component } from "react";
+import { InputNumber, Slider } from "antd";
+import "./ContentBottom.scss";
 class ContentBottom extends Component {
-    static defaultProps = {
-        minNumber:0.175,
-        maxNumber:1.75,
-        step:0.01,
-        value:1,
-    }
-    constructor(props) {
-        super(props);
-    }
+  static defaultProps = {
+    minNumber: 0.17,
+    maxNumber: 1.75,
+    step: 0.01,
+    value: 1
+  };
+  constructor(props) {
+    super(props);
+  }
 
-    setCanvasScaleVal(scaleVal){
-       this.props.setContentScale(scaleVal);    
-    }
+  setCanvasScaleVal = value => {
+    this.props.setContentScale(value);
+  };
 
-    render() { 
-        let props = this.props;
-        let scaleValue = props.value;
-        return ( 
-            <div className="custom-content-bottom-left">
-                <div className="custom-content-bottom-left-item" >
-                     <span>{(scaleValue.toFixed(2)*100)+'%'}</span>
-                </div>
-                <div className="custom-content-bottom-left-item"  style={{width:'100px'}}>
-                    <Slider
-                        min={props.minNumber}
-                        max={props.maxNumber}
-                        step={props.step}
-                        onChange={event => {
-                            this.setCanvasScaleVal(event);
-                        }}
-                        value={typeof scaleValue === 'number' ? scaleValue : 1}
-                    />
-                </div>
-            </div>
-         );
-    }
+  formatter(value) {
+    return parseFloat(value * 100).toFixed(0) + "%";
+  }
+
+  render() {
+    let props = this.props;
+    let scaleValue = props.value;
+    return (
+      <div className="custom-content-bottom-left">
+        <div className="custom-content-bottom-left-item">
+          <span>{parseFloat(scaleValue * 100).toFixed(0) + "%"}</span>
+        </div>
+        <div
+          className="custom-content-bottom-left-item"
+          style={{ width: "100px" }}
+        >
+          <Slider
+            min={props.minNumber}
+            max={props.maxNumber}
+            step={props.step}
+            onChange={this.setCanvasScaleVal}
+            value={typeof scaleValue === "number" ? scaleValue : 1}
+            tipFormatter={this.formatter}
+          />
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default ContentBottom;
